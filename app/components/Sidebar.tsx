8a0f802f74f009b63c1a9c40a4fb9243c4073826
visal <phoung.visal@institute.pse.ngo>
2025-08-25 15:35:00 +0700

    Delete dashboard in sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
       <div className="h-full px-3 pb-4 overflow-y-auto bg-white">
         <ul className="space-y-2 font-medium">
           {/* Dashboard Link */}
-          <li>
+          {/* <li>
             <Link
               href="/dashboard"
               className={`flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group ${
@@ -40,7 +40,7 @@ const Sidebar = () => {
               </svg>
               <span className="ml-3">Dashboard</span>
             </Link>
-          </li>
+          </li> */}
 
           {/* Product Link */}
           <li>
